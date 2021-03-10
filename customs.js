b9189b998dc44586286ef72d22bdf023ed69fe47
Antimin994 <anton.antimijchuk@gmail.com>
2021-03-10 22:45:21 +0200

    first commit

---
+++ customs.js
@@ -0,0 +1,7 @@
+document.querySelector("img").setAttribute("src", "logo.png");
+document.querySelector("h1").setAttribute("style", "color: #666; text-align: left; text-transform: uppercase;");
+document.querySelector("h3").setAttribute("style", "color: #666; text-align: left;");
+
+var createText = document.querySelector("p");
+createText.setAttribute("style", "color: #636;");
+createText.textContent = "Здесь мог бы быть выш отзыв";
